55ec335d80b09c0aa886ebf7cfb60de844343704
awinardi1004 <awinardi1004@gmail.com>
2024-06-10 19:03:38 +0700

    add file

--- dbt-project/include/my_dbt_project/models/dimensional/dim_coupon.sql
+++ dbt-project/include/my_dbt_project/models/dimensional/dim_coupon.sql
@@ -1,6 +1,6 @@
 select
     Coupon_Code as coupon_code,
-    Month as mount_coupon,
+    Month as month_coupon,
     Discount_pct as  discount_pct,
     Product_Category as product_category_coupon
 from {{ ref('stg_disc_kupon') }}
--- dbt-project/include/my_dbt_project/models/dimensional/dim_date.sql
+++ dbt-project/include/my_dbt_project/models/dimensional/dim_date.sql
@@ -1,6 +1,6 @@
-SELECT DISTINCT
+select distinct
     PARSE_DATE('%m/%d/%Y', Transaction_Date) as date,
-    EXTRACT(DAY FROM PARSE_DATE('%m/%d/%Y', Transaction_Date)) AS day,
-    FORMAT_DATE('%b', PARSE_DATE('%m/%d/%Y', Transaction_Date)) AS month,
-    EXTRACT(YEAR FROM PARSE_DATE('%m/%d/%Y', Transaction_Date)) AS year
-FROM {{ ref('stg_online_sales') }}
+    EXTRACT(day from PARSE_DATE('%m/%d/%Y', Transaction_Date)) as day,
+    FORMAT_DATE('%b', PARSE_DATE('%m/%d/%Y', Transaction_Date)) as month,
+    EXTRACT(year from PARSE_DATE('%m/%d/%Y', Transaction_Date)) as year
+from {{ ref('stg_online_sales') }}
